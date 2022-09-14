92f1dc8172978a39aa1648d3da778eeba16fd6f2
JosephR <joseph_rundell@outlook.com>
2022-09-14 02:32:20 -0400

    Final touches for preproduction

--- src/Components/ENSDisplay.js
+++ src/Components/ENSDisplay.js
@@ -18,7 +18,7 @@ export default function ENSDisplay(props){
             return <div className="results">
                 <div className="registryStatus">{props.ensInfo.ensName}.eth:<p className="statusRegistered">Registered</p></div>
                 <img key={props.ensInfo.ensName} className="validityImage" src="https://i.ibb.co/7ptyPRg/redCross.png" alt="A red cross (X)"></img>
-                <p>Owner: {props.ensInfo.ensOwner}</p>
+                <p><a className="ownerAddress" href={`https://etherscan.io/address/${props.ensInfo.ensOwner}`} target="_blank" rel="noreferrer">Owner: {props.ensInfo.ensOwner}</a></p>
                 <div className="resultLinks">
                     <a href={`https://app.ens.domains/name/${props.ensInfo.ensName}.eth`} target="_blank" rel="noreferrer">
                         <img src="https://464911102-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/collections%2F2TjMAeHSzwlQgcOdL48E%2Ficon%2FKWP0gk2C6bdRPliWIA6o%2Fens%20transparent%20background.png?alt=media&token=bd28b063-5a75-4971-890c-97becea09076" className="linkLogo" alt="ens logo"></img>
